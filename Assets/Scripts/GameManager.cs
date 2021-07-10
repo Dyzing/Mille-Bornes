bd071b9cb514c6261db235d771217df2a20f1684
Dyzing <guillaume.trem@outlook.fr>
2021-07-10 02:55:31 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameObject voiture1;
+    public static int KM_1;
+    public static string carteJouée;
+
+    void Start()
+    {
+        voiture1 = GameObject.Find("Voiture" + 1);
+        KM_1 = 0;
+        carteJouée = "";
+    }
+
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (GameManager.carteJouée)
+        {
+            case "KM":
+                MoveVoiture();
+                GameManager.carteJouée = "";
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void MoveVoiture()
+    {
+        Vector3 voiturepos = GameObject.Find("Voiture" + 1).transform.position;
+        Vector3 destination;
+        if (GameManager.KM_1 <= 39)
+        {
+            destination = GameObject.Find("Node" + GameManager.KM_1).transform.position;
+        }
+        else
+        {
+            destination = GameObject.Find("Node40").transform.position;
+        }
+        voiturepos = destination;
+        voiturepos.y += 0.5f;
+        GameManager.voiture1.transform.position = voiturepos;
+        if (GameManager.KM_1 <= 38)
+        {
+            GameManager.voiture1.transform.eulerAngles = GameObject.Find("Node" + (GameManager.KM_1 + 1)).transform.eulerAngles;
+        }
+        else
+        {
+            GameManager.voiture1.transform.eulerAngles = GameObject.Find("Node40").transform.eulerAngles;
+        }
+    }
+}
